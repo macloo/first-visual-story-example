1af7bad62ae8b7bff5ccb99ec687f202a29979e8
Mindy McAdams <mmcadams@gmail.com>
2023-03-04 12:40:52 -0600

    fix map script

--- scripts/app.js
+++ scripts/app.js
@@ -8,42 +8,6 @@ import * as L from 'leaflet';
 import homicides from '../_data/harvard_park_homicides.json';
 
 // Set the id of the div on the page where the map will go
-const divId = undefined;
-
-// Create the map
-const map = L.map(divId, {
-  scrollWheelZoom: false,
-});
-
-// Add a satellite layer
-L.tileLayer(
-  'https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v9/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibGF0aW1lcyIsImEiOiJjanJmNjg4ZzYweGtvNDNxa2ZpZ2lma3Z4In0.g0lYVIEs9Y5QcUOhXactHA',
-  {
-    minZoom: 13,
-  }
-).addTo(map);
-
-// Set the center and zoom
-map.setView([33.983265, -118.306799], 18);
-
-// Load the data
-homicides.forEach((obj) => {
-  L.circleMarker([obj.latitude, obj.longitude]) // As a pin ...
-    .addTo(map)
-    .bindTooltip(obj.first_name + ' ' + obj.last_name); // ... with a tooltip
-});
-
-// Now, below ----
-// Edit app.js to set the divId variable that’s now defined to our id.
-// Make sure to put it in quotes.
-
-// Import dependencies
-import * as L from 'leaflet';
-
-// Import data
-import homicides from '../_data/harvard_park_homicides.json';
-
-// Set the id of the div on the page where the map will go
 const divId = 'map';
 
 // Create the map
@@ -64,7 +28,7 @@ map.setView([33.983265, -118.306799], 18);
 
 // Load the data
 homicides.forEach((obj) => {
-  L.circleMarker([obj.latitude, obj.longitude]) // As a pin ...
+  L.circleMarker([obj.latitude, obj.longitude])
     .addTo(map)
-    .bindTooltip(obj.first_name + ' ' + obj.last_name); // ... with a tooltip
+    .bindTooltip(obj.first_name + ' ' + obj.last_name, { permanent: true });
 });
